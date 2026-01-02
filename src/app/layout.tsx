33abcfa4195742b56350cf2bae8dadd50da3d42b
Omm Pani <ommpani@Omms-MacBook-Air.local>
2026-01-03 00:15:40 +0530

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,8 @@
-import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
-
-// export const metadata: Metadata = {
-//   title: "Feldtstern",
-//   description: "",
-// };
 
 export default function RootLayout({
   children,
